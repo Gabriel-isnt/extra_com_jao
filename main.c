3ba1214e4553644c4b10f47f07979d42b96303d3
At_oa <85297718+Gabriel-isnt@users.noreply.github.com>
2024-09-19 14:05:00 -0300

    Update main.c
    só um teste para ver como que eu faço para usar arquivos próprios no c para resolver o problema do fessor pablo e do fessor victor de algebra linear

--- main.c
+++ main.c
@@ -1,24 +1,60 @@
 #include <stdio.h>
 #include "funcoes.h"
 
-int main(){
-    
-    // entrada usuário
-    double matriz2x2[2][3];
-    double matriz3x3[3][4];
-    
-    
-    for(int l = 0; l < 2; l++){
-        for(int c = 0; c < 3; c++){
-            scanf("%lf", &matriz2x2[l][c]);
+int main() {
+    int dim, i, j;
+
+    // Pergunta ao usuário a dimensão (2 ou 3)
+    printf("Entre com a dimensão do espaço (2 ou 3): ");
+    scanf("%d", &dim);
+
+    if (dim == 2) {
+        float mat2[2][2];
+
+        // Lê os vetores de dimensão 2
+        printf("Entre com os vetores no formato 2x2 (duas colunas, dois vetores):\n");
+        for (i = 0; i < 2; i++) {
+            for (j = 0; j < 2; j++) {
+                printf("Elemento [%d][%d]: ", i, j);
+                scanf("%f", &mat2[i][j]);
+            }
         }
+
+        // Calcula o determinante
+        float det2 = determinante2x2(mat2);
+
+        // Verifica se o determinante é diferente de 0
+        if (det2 != 0) {
+            printf("Os vetores formam uma base em R^2.\n");
+        } else {
+            printf("Os vetores NÃO formam uma base em R^2 (são linearmente dependentes).\n");
+        }
+
+    } else if (dim == 3) {
+        float mat3[3][3];
+
+        // Lê os vetores de dimensão 3
+        printf("Entre com os vetores no formato 3x3 (três colunas, três vetores):\n");
+        for (i = 0; i < 3; i++) {
+            for (j = 0; j < 3; j++) {
+                printf("Elemento [%d][%d]: ", i, j);
+                scanf("%f", &mat3[i][j]);
+            }
+        }
+
+        // Calcula o determinante
+        float det3 = determinante3x3(mat3);
+
+        // Verifica se o determinante é diferente de 0
+        if (det3 != 0) {
+            printf("Os vetores formam uma base em R^3.\n");
+        } else {
+            printf("Os vetores NÃO formam uma base em R^3 (são linearmente dependentes).\n");
+        }
+
+    } else {
+        printf("Dimensão inválida! Escolha 2 ou 3.\n");
     }
 
-    resolve2x2(matriz2x2);
-    
-    
     return 0;
 }
-
-
-
